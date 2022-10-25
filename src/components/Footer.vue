faea15259304f865763c00917cb1591a4a1a4eb2
Lucas <lucasbasoaltocontreras@gmail.com>
2022-10-25 16:47:57 -0300

    bootstrap

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <div>
+     <footer>
+
+     </footer>
+    </div>
+
+</template>
+
+<style>
+    footer {
+    position: fixed;
+    height: 100px;
+    bottom: 0;
+    width: 100%;
+    background-color: blue;
+    color: blanchedalmond;
+}
+</style>
